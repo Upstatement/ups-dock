a5eeff0810c9256e083687465eefeb69a7609ca5
Mike Burns <mike.burns@upstatement.com>
2018-03-11 12:03:58 -0400

    Only generate wp-config.php on first run

--- starter-kits/wordpress/scripts/setup.sh
+++ starter-kits/wordpress/scripts/setup.sh
@@ -9,14 +9,16 @@ until mysql -uwordpress -pwordpress -hdb wordpress; do
 done
 
 # Configure WordPress
-echo "Configuring WordPress..."
-wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST --extra-php <<PHP
+if [ ! -f wp-config.php ]; then
+	echo "Configuring WordPress..."
+	wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST --extra-php <<PHP
 // This allows the WordPress site to run securly behind the ups-dock reverse proxy
 if ( strpos( \$_SERVER['HTTP_X_FORWARDED_PROTO'], 'https' ) !== false ) {
-    \$_SERVER['HTTPS'] = 'on';
+	\$_SERVER['HTTPS'] = 'on';
 }
 define( 'WP_DEBUG', true );
 PHP
+fi
 
 # Run WP install if it's not already installed.
 if ! $(wp core is-installed); then
