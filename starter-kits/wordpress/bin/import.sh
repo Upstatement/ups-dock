dd748954e0ef053cd05340ae917eb3e756c72603
Mike Burns <mike.burns@upstatement.com>
2018-03-13 13:48:32 -0400

    Update import script

--- starter-kits/wordpress/bin/import.sh
+++ starter-kits/wordpress/bin/import.sh
@@ -3,6 +3,6 @@
 set -e
 
 echo "Importing database..."
-docker-compose exec wordpress wp --allow-root db import dbdump.sql
+docker-compose exec wordpress wp db import dbdump.sql
 echo "Replacing live URLs with localURLs"
-docker-compose exec wordpress wp --allow-root search-replace www.OLDHOST.com NEWHOST.ups.dock
+docker-compose exec wordpress wp search-replace www.OLDHOST.com NEWHOST.ups.dock
